3009b53f1da68a6cfc008ebd11dfe8bfcc12ab36
Andrew102803 <106626454+Andrew102803@users.noreply.github.com>
2022-07-21 18:09:29 -0500

    Update script.js
    adds sekector for todays weather as well as the 5 day forecast, code tpo determine temp, what it feels like, humid and uv as well as lat and long  and winf, and it sets color for the uv warinibgs

--- script.js
+++ script.js
@@ -48,3 +48,64 @@ function getHistory() {
 		getWeatherToday();
 	});
 };
+//Today weather list thng selctoert 
+var cardTodayBody = $('.cardBodyToday')
+//launches the 5 day planner weather thing
+function getWeatherToday() {
+	var getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${maincity}&units=imperial&appid=${authorizationthingy}`;
+
+	$(cardTodayBody).empty();
+
+	$.ajax({
+		url: getUrlCurrent,
+		method: 'GET',
+	}).then(function (response) {
+		$('.cardTodayCityName').text(response.name);
+		$('.cardTodayDate').text(date);
+		//Icons
+		$('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
+		// what iyt offically is
+		var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
+		cardTodayBody.append(pEl);
+		//Humidity self explanitory
+		var humidityfunny = $('<p>').text(`Humidity: ${response.main.humidity} %`);
+		cardTodayBody.append(humidityfunny);
+		//what it de facto be aka catuallt is
+		var tempfunny = $('<p>').text(`Feels Like: ${response.main.feels_like} °F`);
+		cardTodayBody.append(tempfunny);
+		//Wind Speed
+		var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
+		cardTodayBody.append(pElWind);
+		//latitude and longitude
+		var cityLat = response.coord.lat;
+		// same as below but latitude
+		var cityLon = response.coord.lon;
+		// logs the longitude of city
+// nose blkeeds
+		var getuithing = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=hourly,daily,minutely&appid=${authorizationthingy}`;
+
+		$.ajax({
+			url: getuithing,
+			method: 'GET',
+		}).then(function (response) {
+			var uvindexthingy = $('<p>').text(`UV Index: `);
+			var seconduvthingy = $('<span>').text(response.current.uvi);
+			var uvi = response.current.uvi;
+			uvindexthingy.append(seconduvthingy);
+			cardTodayBody.append(uvindexthingy);
+			//uv index now has color to make you scared of the unfathomable power of the sun as your skin is burned by UV lights as we destroy our ozozne layer
+			if (uvi >= 0 && uvi <= 2) {
+				seconduvthingy.attr('class', 'green');
+			} else if (uvi > 2 && uvi <= 5) {
+				seconduvthingy.attr("class", "yellow")
+			} else if (uvi > 5 && uvi <= 7) {
+				seconduvthingy.attr("class", "orange")
+			} else if (uvi > 7 && uvi <= 10) {
+				seconduvthingy.attr("class", "red")
+			} else {
+				seconduvthingy.attr("class", "purple")
+			}
+		});
+	});
+	getFiveDayForecast();
+};
